408ba2017abc5e2f9017a49e064f9603bd86298c
Joonas <joonas.valkama@eneroc.com>
2025-01-29 14:58:09 +0200

    add working dir setting snippet

---
+++ scripts/python/set_working_dir.py
@@ -0,0 +1,10 @@
+"""
+Use this snippet to set the working directory to the scripts own directory.
+This is usefull when the code may be ran from anywhere and uses relative file paths.
+"""
+
+import os # Import os module to access filesystem info
+
+abspath = os.path.abspath(__file__) # Get absolute path of current file
+dname = os.path.dirname(abspath) # Get directory component of current file (using absolute path)
+os.chdir(dname) # Change working directory to the directory this file is in
